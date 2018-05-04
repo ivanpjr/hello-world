daef4a32a74bc3118f10ef6bb71836d37911cfc3
Ivan Pinheiro <ivanpjr@yahoo.com>
2018-05-04 10:04:18 +0200

    Swedish option included

--- c++/Calendar/Calendar.cpp
+++ c++/Calendar/Calendar.cpp
@@ -40,13 +40,14 @@ void printCalendar (int year, int month, const string& ws);
 
 void showHelp(){
     cout << "Syntax:" << endl
-         << "./Calendar [-l] [-h] [-m month] [-y year] [-nm num_monts] [-ws week_start]" << endl
+         << "./Calendar [-l] [-h] [-m month] [-y year] [-nm num_monts] [-ws week_start] [-sv]" << endl
          << "\t-l            \tEnable logging (debug purposes)" << endl
          << "\t-h            \tShows this help" << endl
          << "\t-m month      \tMonth for the Calendar in numeric value, e.g.: -m 5. Must be 0 < value <= 12." << endl
          << "\t-y year       \tYear for the Calendar in numeric value, e.g.: -y 2018. Must be -1 < value." << endl
          << "\t-nm num_months\tNumber of months to print out in numeric value, e.g.: -nm 3. Must be 0 < value <= 24." << endl
          << "\t-ws week_start\tWeek starting day in string value, default is Sun. Allowed values: [Sun|Mon]." << endl
+         << "\t-sv           \tSkriv ut Calendar i svenska. Default is english." << endl
          << endl;
     return;
 }
@@ -54,22 +55,8 @@ void showHelp(){
 //Initialize stuff
 bool log=false; //debugging logs
 
-map<int,string> weekDays;
-
-map<int,pair<string, int>> months{  // Months and days per month
-        {1,make_pair("January", 31)},
-        {2,make_pair("February", 28)},
-        {3,make_pair("March", 31)},
-        {4,make_pair("April", 30)},
-        {5,make_pair("May", 31)},
-        {6,make_pair("June", 30)},
-        {7,make_pair("July", 31)},
-        {8,make_pair("August", 31)},
-        {9,make_pair("September", 30)},
-        {10,make_pair("October", 31)},
-        {11,make_pair("November", 30)},
-        {12,make_pair("December", 31)}
-};
+map<int,string> weekDays;  // Days of the week
+map<int,pair<string, int>> months;  // Months and days per month
 
 int main(int argc, char* argv[]) {
 
@@ -81,6 +68,7 @@ int main(int argc, char* argv[]) {
     int month = now->tm_mon + 1;  //tm_mon is 0-based, adjust by adding 1
     int numMonths = 1; // default is to print out only one month
     string ws = "Sun"; // default is to start on Sunday (0)
+    bool sv = false; // flag for language: English (false) or Swedish (true)
 
 // Treat arguments
     try {
@@ -88,7 +76,7 @@ int main(int argc, char* argv[]) {
 
             string arg = argv[k];
 
-            if (arg != "-l" && arg != "-h" && arg != "-m" && arg != "-y" && arg != "-nm" && arg != "-ws") {
+            if (arg != "-l" && arg != "-h" && arg != "-m" && arg != "-y" && arg != "-nm" && arg != "-ws" && arg != "-sv") {
                 throw invalid_argument(arg);
 
             } else if (arg == "-h") {
@@ -119,6 +107,9 @@ int main(int argc, char* argv[]) {
                     throw invalid_argument(arg);
                 }
 
+            } else if (arg == "-sv") {
+                sv = true;
+
             } else if (arg == "-l") {
                 log = true;
             }
@@ -140,45 +131,86 @@ int main(int argc, char* argv[]) {
                   <<  now->tm_wday << "."
                   << "\n";
 
+// Fix the language: English or Swedish
+    string jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec;
+    jan= sv? "Januari"   : "January"   ;
+    feb= sv? "Februari"  : "February"  ;
+    mar= sv? "Mars"      : "March"     ;
+    apr= sv? "April"     : "April"     ;
+    may= sv? "Maj"       : "May"       ;
+    jun= sv? "Juni"      : "June"      ;
+    jul= sv? "Juli"      : "July"      ;
+    aug= sv? "Augusti"   : "August"    ;
+    sep= sv? "September" : "September" ;
+    oct= sv? "Oktober"   : "October"   ;
+    nov= sv? "November"  : "November"  ;
+    dec= sv? "December"  : "December"  ;
+
+    string sun, mon, tue, wed, thu, fri, sat;
+    sun= sv? "Sön" : "Sun" ;
+    mon= sv? "Mån" : "Mon" ;
+    tue= sv? "Tis" : "Tue" ;
+    wed= sv? "Ons" : "Wed" ;
+    thu= sv? "Tor" : "Thu" ;
+    fri= sv? "Fre" : "Fri" ;
+    sat= sv? "Lör" : "Sat" ;
+
+//Fill in month names and days per month
+    months = {
+            {1,make_pair(jan, 31)},
+            {2,make_pair(feb, 28)},  // days in February will be adjusted later, in case of leap years
+            {3,make_pair(mar, 31)},
+            {4,make_pair(apr, 30)},
+            {5,make_pair(may, 31)},
+            {6,make_pair(jun, 30)},
+            {7,make_pair(jul, 31)},
+            {8,make_pair(aug, 31)},
+            {9,make_pair(sep, 30)},
+            {10,make_pair(oct, 31)},
+            {11,make_pair(nov, 30)},
+            {12,make_pair(dec, 31)}
+    };
+
 //Fix the offset for week starting day
     if (ws == "Sun") {
         weekDays = {  //Days of the Week
-                {0,"Sun"},
-                {1,"Mon"},
-                {2,"Tue"},
-                {3,"Wed"},
-                {4,"Thu"},
-                {5,"Fri"},
-                {6,"Sat"}
+                {0,sun},
+                {1,mon},
+                {2,tue},
+                {3,wed},
+                {4,thu},
+                {5,fri},
+                {6,sat}
         };
     }
     else if (ws == "Mon")  {
         weekDays = {  //Days of the Week
-                {0,"Mon"},
-                {1,"Tue"},
-                {2,"Wed"},
-                {3,"Thu"},
-                {4,"Fri"},
-                {5,"Sat"},
-                {6,"Sun"}
+                {0,mon},
+                {1,tue},
+                {2,wed},
+                {3,thu},
+                {4,fri},
+                {5,sat},
+                {6,sun}
         };
     }
 
-//Fix the number of days in february for leap years
-    int daysFeb;
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-        daysFeb = 29;
-    else
-        daysFeb = 28;
-    months.find(2)->second.second = daysFeb;
-    if (log) clog << "daysFeb: " << daysFeb << endl;
 
-//Print out calendar
-    for (int m=1; m<=numMonths; m++){
+//Print out calendar(s)
+    for (int m=1; m<=numMonths; m++){  // Loop in case more than one month is to be printed out
+
         if(month>12) {  // If December is passed, increment the year and reset the month to January
             year++;
             month=1;
         }
+
+        //Fix the number of days in february for leap years
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            months.find(2)->second.second = 29;
+        else
+            months.find(2)->second.second = 28;
+
+        if (log) clog << "Days in February in " << year << ": " << months.find(2)->second.second << endl;
         if (log) clog << "Calling printCalendar( " << year << ", " << month << " )" << endl;
         printCalendar(year, month++, ws);
     }
@@ -211,18 +243,18 @@ void printCalendar (int year, int month, const string& ws){
 //Print out days, in 7 columns
     int col;
     if (log) cout << "dow: " << dow << endl;
-    if (ws == "Mon") {
-        if (dow == 0)
-            dow = 6;
+    if (ws == "Mon") {  // If week starts on Monday
+        if (dow == 0)   //   If first day of the month is Sunday (0)
+            dow = 6;    //     Make it 6 so 6 tabs are printed out before it (further below)
         else
-            dow--;
+            dow--;      //     Otherwise decrement
     }
-    for (col=1; col<=dow; col++){
+    for (col=1; col<=dow; col++){ // Print empty tabs before first day of the month
         cout << "\t";
     }
-    for(int d=1; d<=it->second.second; d++) {
+    for(int d=1; d<=it->second.second; d++) {  // Print days in columns separated by tab
         cout << d << "\t";
-        if (col%7 == 0) cout << endl;
+        if (col%7 == 0) cout << endl; // Break line after every 7th column (when col is multiple of 7)
         col++;
     }
 
