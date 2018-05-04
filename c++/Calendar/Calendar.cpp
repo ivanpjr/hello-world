4d85f7e59c761963c158d0601be3ac26c2a83951
Ivan Pinheiro <ivanpjr@yahoo.com>
2018-05-04 16:23:33 +0200

    Support for any week starting day included.

--- c++/Calendar/Calendar.cpp
+++ c++/Calendar/Calendar.cpp
@@ -34,9 +34,11 @@ If you have some spare time, you can add options to
 #include <ctime>
 #include <map>
 #include <stdexcept>
+#include <vector>
+
 using namespace std;
 
-void printCalendar (int year, int month, const string& ws);
+void printCalendar (int year, int month, int ws);
 
 void showHelp(){
     cout << "Syntax:" << endl
@@ -46,7 +48,7 @@ void showHelp(){
          << "\t-m month      \tMonth for the Calendar in numeric value, e.g.: -m 5. Must be 0 < value <= 12." << endl
          << "\t-y year       \tYear for the Calendar in numeric value, e.g.: -y 2018. Must be -1 < value." << endl
          << "\t-nm num_months\tNumber of months to print out in numeric value, e.g.: -nm 3. Must be 0 < value <= 24." << endl
-         << "\t-ws week_start\tWeek starting day in string value, default is Sun. Allowed values: [Sun|Mon]." << endl
+         << "\t-ws week_start\tWeek starting day in numeric value, default is 0. Allowed values: [0:Sun|1:Mon|2:Tue|3:Wed|4:Thu|5:Fri|6:Sat]." << endl
          << "\t-sv           \tSkriv ut Calendar i svenska. Default is english." << endl
          << endl;
     return;
@@ -54,7 +56,6 @@ void showHelp(){
 
 //Initialize stuff
 bool log=false; //debugging logs
-
 map<int,string> weekDays;  // Days of the week
 map<int,pair<string, int>> months;  // Months and days per month
 
@@ -67,7 +68,7 @@ int main(int argc, char* argv[]) {
     int year = now->tm_year + 1900;  //tm_year is 1900-based
     int month = now->tm_mon + 1;  //tm_mon is 0-based, adjust by adding 1
     int numMonths = 1; // default is to print out only one month
-    string ws = "Sun"; // default is to start on Sunday (0)
+    int ws = 0; // default is to start on Sunday (0)
     bool sv = false; // flag for language: English (false) or Swedish (true)
 
 // Treat arguments
@@ -102,8 +103,8 @@ int main(int argc, char* argv[]) {
                 }
 
             } else if (arg == "-ws") {
-                ws = argv[++k];
-                if (ws != "Sun" && ws != "Mon") {
+                ws = static_cast<int>(stoi(argv[++k]));
+                if ((ws < 0) || (ws > 6)) {
                     throw invalid_argument(arg);
                 }
 
@@ -146,15 +147,6 @@ int main(int argc, char* argv[]) {
     nov= sv? "November"  : "November"  ;
     dec= sv? "December"  : "December"  ;
 
-    string sun, mon, tue, wed, thu, fri, sat;
-    sun= sv? "Sön" : "Sun" ;
-    mon= sv? "Mån" : "Mon" ;
-    tue= sv? "Tis" : "Tue" ;
-    wed= sv? "Ons" : "Wed" ;
-    thu= sv? "Tor" : "Thu" ;
-    fri= sv? "Fre" : "Fri" ;
-    sat= sv? "Lör" : "Sat" ;
-
 //Fill in month names and days per month
     months = {
             {1,make_pair(jan, 31)},
@@ -171,29 +163,23 @@ int main(int argc, char* argv[]) {
             {12,make_pair(dec, 31)}
     };
 
+    vector<string> week;
+    if (sv)
+        week = {"Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"};
+    else
+        week = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
 //Fix the offset for week starting day
-    if (ws == "Sun") {
-        weekDays = {  //Days of the Week
-                {0,sun},
-                {1,mon},
-                {2,tue},
-                {3,wed},
-                {4,thu},
-                {5,fri},
-                {6,sat}
-        };
-    }
-    else if (ws == "Mon")  {
-        weekDays = {  //Days of the Week
-                {0,mon},
-                {1,tue},
-                {2,wed},
-                {3,thu},
-                {4,fri},
-                {5,sat},
-                {6,sun}
-        };
-    }
+    string day0=week[ws];
+    string day1=week[ws<6?ws+1:ws-6];
+    string day2=week[ws<5?ws+2:ws-5];
+    string day3=week[ws<4?ws+3:ws-4];
+    string day4=week[ws<3?ws+4:ws-3];
+    string day5=week[ws<2?ws+5:ws-2];
+    string day6=week[ws<1?ws+6:ws-1];
+    weekDays = {{0,day0},{1,day1},{2,day2},{3,day3},{4,day4},{5,day5},{6,day6}};
+
+    if (log) clog <<  "Week: " << day0 << "," << day1 << "," << day2 << "," << day3 << "," << day4 << "," << day5 << "," << day6 << endl;
 
 
 //Print out calendar(s)
@@ -211,7 +197,7 @@ int main(int argc, char* argv[]) {
             months.find(2)->second.second = 28;
 
         if (log) clog << "Days in February in " << year << ": " << months.find(2)->second.second << endl;
-        if (log) clog << "Calling printCalendar( " << year << ", " << month << " )" << endl;
+        if (log) clog << "Calling printCalendar( " << year << ", " << month << ", " << ws << " )" << endl;
         printCalendar(year, month++, ws);
     }
 
@@ -219,13 +205,15 @@ int main(int argc, char* argv[]) {
 }
 
 
-void printCalendar (int year, int month, const string& ws){
+void printCalendar (int year, int month, int ws){
+    if (log) clog << "Inside printCalendar( " << year << ", " << month << ", " << ws << " )" << endl;
 
 //Calculate day of the week based on input values for month and year, or current values if no input
     tm inputDate = { 0, 0, 0, 1, month - 1, year - 1900 }; // second, minute, hour, 1-based day, 0-based month, year since 1900
     time_t tmpDate = mktime(&inputDate);
     const tm * then = localtime(&tmpDate);
     int dow = then->tm_wday; // 0-based Day of the Week (dow)
+    if (log) cout << "Day of the week for first day of month: " << dow << endl;
 
 //Skip one line
     cout << endl;
@@ -240,18 +228,24 @@ void printCalendar (int year, int month, const string& ws){
         cout << d.second << "\t";
     cout << endl;
 
-//Print out days, in 7 columns
-    int col;
-    if (log) cout << "dow: " << dow << endl;
-    if (ws == "Mon") {  // If week starts on Monday
-        if (dow == 0)   //   If first day of the month is Sunday (0)
-            dow = 6;    //     Make it 6 so 6 tabs are printed out before it (further below)
-        else
-            dow--;      //     Otherwise decrement
+//Adjust offset (tabs to be printed) before the first day of the month, depending on the week starting day
+    switch (dow) {
+        case 0: ws <= 0? dow=dow-ws: dow=dow+(7-ws); break;
+        case 1: ws <= 1? dow=dow-ws: dow=dow+(7-ws); break;
+        case 2: ws <= 2? dow=dow-ws: dow=dow+(7-ws); break;
+        case 3: ws <= 3? dow=dow-ws: dow=dow+(7-ws); break;
+        case 4: ws <= 4? dow=dow-ws: dow=dow+(7-ws); break;
+        case 5: ws <= 5? dow=dow-ws: dow=dow+(7-ws); break;
+        case 6: ws <= 6? dow=dow-ws: dow=dow+0; break;
     }
+    if (log) cout << "Day of the week for first day of month (adjusted): " << dow << endl;
+
+    //Print out days, in 7 columns
+    int col;
     for (col=1; col<=dow; col++){ // Print empty tabs before first day of the month
         cout << "\t";
     }
+
     for(int d=1; d<=it->second.second; d++) {  // Print days in columns separated by tab
         cout << d << "\t";
         if (col%7 == 0) cout << endl; // Break line after every 7th column (when col is multiple of 7)
